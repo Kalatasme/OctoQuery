bde0d1dddcd83cb525ea0b5ae943032740c36e51
Kalatasme <michaelroyvarley@outlook.com>
2022-12-18 16:47:23 +0000

    Initial Push

---
+++ octo_gas_consume.py
@@ -0,0 +1,99 @@
+import requests
+import json
+from datetime import datetime, timedelta
+
+#
+# DATES & TIMES
+# Consumption API accepts earliest/latest snapped to half hour increments (HH:00 - HH:30 & HH:30 - HH:00).
+# This is where we calculate those values to be passed to the API
+#
+
+# Get the current system time.
+timenow = datetime.now()
+#timenow = "2022-11-12 14:29:15.535784"
+# Print timenow
+#print(timenow)
+
+# Testing - Only needed if timenow is manually set.  Turns from string to datetime format.
+#datetime_time = datetime.strptime(timenow, "%Y-%m-%d %H:%M:%S.%f")
+#print(datetime_time)
+
+# Create our rounded time, snaps down to the nearest 30 mins.  
+# Mins 01-29 = 00
+# Mins 31-59 = 30
+rounded = timenow - (timenow - datetime.min) % timedelta(minutes=30)
+# Print rounded time.
+#print(rounded)
+
+# Octopus API wants period_from & period_to values in ISO 8601 Format.
+iso_timenow = rounded.isoformat()
+
+# I get errors here about string & datetime values not gelling, so I'm going to force it into a format that can be manipulated.
+date_format = '%Y-%m-%dT%H:%M:%S'
+provided_time = datetime.strptime(iso_timenow, date_format)
+#print(provided_time)
+
+# period_to value.  Take our rounded time, subtract a minute for our 29 or 59 value.
+#latest = "{}Z".format(provided_time - timedelta(minutes=1)).replace(" ", "T")
+#print(latest)
+
+# period_from value.  Take our rounded time, subtract 30 mins for our 00 or 30 value.
+#earliest = "{}Z".format(provided_time - timedelta(minutes=30)).replace(" ", "T")
+#print(earliest)
+
+# Testing Specific Periods
+earliest = "2022-11-08T00:00:00Z"
+latest = "2022-11-08T23:59:00Z"
+
+
+
+time_period = f"?period_from={earliest}&period_to={latest}"
+#print(time_period)
+
+
+# Variables
+
+# https://octopus.energy/dashboard/developer/
+base_url = "https://api.octopus.energy"
+api_key = XXXXX
+
+# https://www.base64encode.org/
+enc_api_key = XXXXX
+
+# https://octopus.energy/dashboard/developer/
+elec_mpan = XXXXX
+elec_serial = XXXXX
+
+# https://octopus.energy/dashboard/developer/
+gas_mprn = XXXXX
+gas_serial = XXXXX
+
+# Build Header
+# Basic Auth as per https://developer.octopus.energy/docs/api/
+header = {"Authorization": f"Basic {enc_api_key}"}
+
+# Get a list of products
+response_gas_consume = requests.get(f"{base_url}/v1/gas-meter-points/{gas_mprn}/meters/{gas_serial}/consumption/{time_period}", headers=header)
+
+# Get current Electricity Consumption
+# response_elec_consumption = requests.get(f"{base_url}/v1/electricity-meter-points/{elec_mpan}/meters/{elec_serial}/consumption", headers=header)
+
+# get response body as json. If the body text is not a json string, raise a ValueError
+rp_json = response_gas_consume.json()
+
+# get response status code
+rp_status = response_gas_consume.status_code
+
+# check the response status, if the status is not sucessful, raise requests.HTTPError
+response_gas_consume.raise_for_status()
+
+# Strip the header - Print only the contents of the Results dictionary.
+rgc_json_string = json.dumps(rp_json["results"])
+
+# Let's just double-check this is actually JSON before it hits Splunk.
+# I have been burned before.
+json.loads(rgc_json_string)
+
+# Let's output our JSON payload.
+# For now we'll print and output to a file via the CLI for manual ingestion into Splunk.
+print(rgc_json_string)
--- octo_products.py
+++ octo_products.py
@@ -5,19 +5,18 @@ import json
 
 # https://octopus.energy/dashboard/developer/
 base_url = "https://api.octopus.energy"
-api_key = "sk_live_sjFqMHi3PHCmQfGJyRKjEwPY"
+api_key = XXXXX
 
 # https://www.base64encode.org/
-enc_api_key = "c2tfbGl2ZV9zakZxTUhpM1BIQ21RZkdKeVJLakV3UFk="
+enc_api_key = XXXXX
 
 # https://octopus.energy/dashboard/developer/
-elec_mpan = "3200000070992"
-elec_serial = "21E5433813"
+elec_mpan = XXXXX
+elec_serial = XXXXX
 
 # https://octopus.energy/dashboard/developer/
-gas_mprn = "7621831004"
-gas_serial = "E6E06453952221"
-
+gas_mprn = XXXXX
+gas_serial = XXXXX
 
 # Build Header
 # Basic Auth as per https://developer.octopus.energy/docs/api/
@@ -27,37 +26,24 @@ header = {"Authorization": f"Basic {enc_api_key}"}
 response_products = requests.get(f"{base_url}/v1/products/", headers=header)
 
 # Get current Electricity Consumption
-response_elec_consumption = requests.get(f"{base_url}/v1/electricity-meter-points/{elec_mpan}/meters/{elec_serial}/consumption", headers=header)
-
+# response_elec_consumption = requests.get(f"{base_url}/v1/electricity-meter-points/{elec_mpan}/meters/{elec_serial}/consumption", headers=header)
 
 # get response body as json. If the body text is not a json string, raise a ValueError
 rp_json = response_products.json()
 
 # get response status code
 rp_status = response_products.status_code
+
 # check the response status, if the status is not sucessful, raise requests.HTTPError
 response_products.raise_for_status()
 
-rec_json = response_elec_consumption.json()
-rec_status = response_elec_consumption.status_code
-response_elec_consumption.raise_for_status()
-
-
-#print(rp_json)
-
+# Strip the header - Print only the contents of the Results dictionary.
 rp_json_string = json.dumps(rp_json["results"])
+
+# Let's just double-check this is actually JSON before it hits Splunk.
+# I have been burned before.
 json.loads(rp_json_string)
 
-# rp_json_string_fixed = rp_json_string.replace("false", '"false"')
+# Let's output our JSON payload.
+# For now we'll print and output to a file via the CLI for manual ingestion into Splunk.
 print(rp_json_string)
-
-#print(rp_status)
-
-#print(rec_json)
-#print(rec_status)
-
-
-
-
-#print(response.status_code)
-#print(response.json())
